3e268ecab28217ac9cd2c5875d0bc8dd1c34359d
Juan Garcia <jgarc210@gmail.com>
2020-10-30 00:22:44 -0500

    x

---
+++ config.js
@@ -0,0 +1,35 @@
+
+const cfg = [];
+
+//Discord config
+cfg.discord = {
+    token: 'NzcxNTcxNDk4MDQxMjEyOTgw.X5uD9A.Wfugxuxnvu6iTvuPW-SN5JYxIbg',
+    prefix: '!',
+    admin: [
+        '771571498041212980'
+    ]
+};
+
+//General Options
+cfg.options = {
+    defaultVolume: 100 //Sets the default volume percent on join
+};
+
+cfg.api = {
+    youtube: 'AIzaSyBarRwPpT6aXyes_d4O2BdZ2tvai0Rt6sI'
+}
+
+cfg.lavalink = {
+    node: {
+        host: '127.0.0.1',
+        port: 6060,
+        password: 'Vanossgames210'
+    },
+    rest: {
+        host: '127.0.0.1',
+        port: 6060,
+        password: 'Vanossgames210'
+    }
+};
+
+module.exports = cfg;
